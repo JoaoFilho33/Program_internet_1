4962f00dbf3444cb7addd2bcb76cd87523996646
João de Deus <joaod.almeidafi@gmail.com>
2023-02-28 11:22:19 -0300

    ideia para buscar em depth

--- buscador/main.py
+++ buscador/main.py
@@ -2,14 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 #res =  requests.get('http://www.ifpi.edu.br/')
+
+#url = input("Infome uma url: ")
+#res = requests.get(url)
 res = requests.get('http://127.0.0.1:5500/html/main.html')
 soup = BeautifulSoup(res.content, 'html.parser')
-
 text = soup.get_text()
 
 key = "dragão"
 
-def busca_termo(key):
+def busca_termo(key, text):
     array = []
 
     for i in range(len(text)):
@@ -23,14 +25,26 @@ def busca_termo(key):
 
 print(busca_termo(key))
 
+def obter_texto(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    text = soup.get_text()
+
+    return text
+
 def search(key, url, depth):
-    links = []
+    text = obter_texto(url)
+    array_text = []
 
     if depth > 0:
         for link in soup.find_all("a"):
             href = link.get('href')
-            links.append(href)
-    
-        busca_termo(key)
+            #links.append(href)
+            array_text.append(obter_texto(href))
+            busca_termo(key, array_text[link])
+    else:
+        busca_termo(key, text)
+        
     #preciso buscar os links da página 0 + links de cada página até chegar no depth escolhido
     #cuidado com requisições de paginas identicas
+
