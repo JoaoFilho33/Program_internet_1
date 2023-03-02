c4cc2a499a810be400d77f5b6b152ce17188cf7a
João de Deus <joaod.almeidafi@gmail.com>
2023-03-02 05:34:02 -0300

    adicionando mecanismo de ranqueamento das páginas

--- buscador/main.py
+++ buscador/main.py
@@ -1,22 +1,9 @@
 import requests 
 import requests_cache
 from bs4 import BeautifulSoup
-import logging
 from urllib.parse import urljoin
 
-#res =  requests.get('http://www.ifpi.edu.br/')
-
-#url = input("Infome uma url: ")
-#res = requests.get(url)
-"""
-res = requests.get('http://127.0.0.1:5500/html/main.html')
-soup = BeautifulSoup(res.content, 'html.parser')
-text = soup.get_text()
-"""
-
-key = "dragão"
-
-def busca_termo(key, text):
+def busca_keyword(key, text):
     array = []
 
     for i in range(len(text)):
@@ -28,54 +15,19 @@ def busca_termo(key, text):
 
     return array
 
-# print(busca_termo(key))
-
-def obter_texto(url):
-    res = requests.get(url)
-    soup = BeautifulSoup(res.content, 'html.parser')
-    text = soup.get_text()
-
-    return text
-
-
-def solicitacao_http(url):
-    return requests.get(url)
-
-def obter_html(url):
-    res = requests.get(url)
-    return BeautifulSoup(res.content, "html.parser")
-
-"""
-def buscar_links(url):
-    soup = obter_html(url)
-    links = soup.find_all("a")
-
-    for link in links:
-            href = link.get('href')
-            if href.startswith('http'):
-                buscar_links(href)
-            else:
-                base_url = requests.get(url).url
-                absolute_url = 
-"""
-
-
-def eh_link_externo_nao_visitado(href, urls_visitadas):
-    if href and href.startswith('http') and href not in urls_visitadas:
-        return True
-
 class localizador_de_links:
-    def __init__(self, start_url, max_depth=3):
-        self.visited_urls = set() # guarda as urls visitadas
+    def __init__(self, start_url, max_depth):
+        self.visited_urls = [] # guarda as urls visitadas
         self.start_url = start_url
         self.max_depth = max_depth
         self.session = None
+        self.references = {}
+
 
     def get_links(self, url):
         session = self.get_session()
         res = session.get(url)
         soup = BeautifulSoup(res.content, 'html.parser')
-        #links = soup.find_all("a")
 
         links = []
         for link in soup.find_all("a"):
@@ -83,9 +35,15 @@ class localizador_de_links:
 
             if href is not None:
                 absolute_url = self.absolute_url(href, res.url)
+
                 if absolute_url not in self.visited_urls:
                     links.append(absolute_url)
+                    # trata o numero de referencias para a url
+                if absolute_url not in self.references:
+                    self.references[absolute_url] = 0
 
+                self.references[absolute_url] += 1
+                        
         return links
 
     def get_session(self): # faz com que armazene em cache as páginas da web que foram acessadas anteriormente 
@@ -97,104 +55,48 @@ class localizador_de_links:
     def absolute_url(self, url, base_url):
         return urljoin(base_url, url)
 
-    """
-    def absolute_url(self, url):
-        session = self.get_session()
-        res = session.get(url)
-        soup = BeautifulSoup(res.content, 'html.parser')
-        array_links = []
-
-        for link in soup.find_all("a"): # encontrar as tags <a>
-            href = link.get('href')
-            if href is not None:
-                absolute_url = self.absolute_url(href, res.url) # criate url absoluta
-                if absolute_url not in self.visited_urls:
-                    array_links.append(absolute_url)
-        return array_links
-    """
-
     def search(self, url=None, depth=0):
         if not url:
             url = self.start_url
         if depth > self.max_depth:
             return [] # se atingir o depth max return lista vazia
+      
+        
         links = self.get_links(url)
-        self.visited_urls.add(url) # added url atual as visitadas
 
-        results = []
-        for link in links:
+        self.visited_urls.append(url) # added url atual as visitadas
+
+        links_ordenados = []
+        # sorted() ordena os links em ordem decrescente de acordo com a qtd de referência de cada um
+        for link in sorted(links, key=lambda x: self.references[x], reverse=True):
             if link not in self.visited_urls:
-                results.append(link)
-                results.extend(self.search(link, depth+1))
+                links_ordenados.append(link)
+                links_ordenados.extend(self.search(link, depth+1))
 
-        return results
-        
-
-    """
-    def buscar_links(self, url, max_depth=0, depth=0):
-        array_links = []
-        self.visited_urls.add(url)
-        res = requests.get(url)
-        soup = BeautifulSoup(res.content, 'html.parser')
-
-        links = soup.find_all("a", href==True)
-
-        if res.status_code==200:
-            text = soup.get_text()
-
-            for link in links:
-                href = link.get('href')
-
-                if eh_link_externo_nao_visitado(href, self.visited_urls):
-                    array_links.append(href)
-                    self.buscar_links(href)
-
-                elif href: # trata links relativos
-                    absolute_url = f'{res.url.rstrip("/")}/{href.lstrip("/")}' # rstrip remove caracteres
-
-                    if absolute_url not in self.visited_urls:
-                        array_links.append(href)
-                        self.buscar_links(absolute_url)
-        
-                
-                    if depth < max_depth:
-                        array_links.extend(buscar_links())
-
-        return array_links
-        """
-
-url = "http://127.0.0.1:5500/buscador/html/main.html"
-findLinks = localizador_de_links(url, max_depth=2)
-results = findLinks.search()
-print(len(results))
-for link in results:
-    print(link)
-
-"""
-def search(key, url, depth):
-    busca = localizador_de_links(url)
-
-    text = obter_texto(url)
-    soup = obter_html(url)
-    array_text = []
-    links = soup.find_all("a") # pega todos os links da página
-
-    if depth > 0:
-        
-        for link in links:
-            href = link.get('href')
-            if href.startswith('http'):
-                array_text.append(obter_texto(href))
-                
-                busca_termo(key, array_text[link])
-        
-        for i in busca:
-            busca.buscar_links(url)
-    else:
-        busca_termo(key, text)
-"""
-    #preciso buscar os links da página 0 + links de cada página até chegar no depth escolhido
-    #cuidado com requisições de paginas identicas
+        return links_ordenados
 
 
+# url = "http://127.0.0.1:5500/buscador/html/main.html"
+url = input("informe a url inicial: ")
+key = input("Palavra-chave da busca: ")
+depthStr = input("Profundidade da busca: ")
+depth = int(depthStr)
 
+findLinks = localizador_de_links(url, depth)
+links = findLinks.search()
+
+# for page in findLinks.visited_urls:
+for page in findLinks.visited_urls:
+    res = requests.get(page)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    text = soup.get_text()
+    ocorrencias = busca_keyword(key, text)
+
+    print()
+    print(f"\nLink: {page}")
+    print("Contexto das ocorrencias: ")
+    for ocorrencia in ocorrencias:
+        print(f"- {ocorrencia}")
+ 
+    print()
+    print(f"Qtd de links referenciando essa página: {findLinks.references[page]}")
---
+++ buscador/teste.py
@@ -0,0 +1,199 @@
+import requests 
+import requests_cache
+from bs4 import BeautifulSoup
+import logging
+from urllib.parse import urljoin
+
+#res =  requests.get('http://www.ifpi.edu.br/')
+
+#url = input("Infome uma url: ")
+#res = requests.get(url)
+"""
+res = requests.get('http://127.0.0.1:5500/html/main.html')
+soup = BeautifulSoup(res.content, 'html.parser')
+text = soup.get_text()
+"""
+
+key = "dragão"
+
+def busca_termo(key, text):
+    array = []
+
+    for i in range(len(text)):
+        if text[i:i+len(key)] == key:
+            inicio = max(0, i - 15)
+            fim = min(len(text), i+len(key) + 15)
+            contexto = text[inicio:fim]
+            array.append(contexto)
+
+    return array
+
+# print(busca_termo(key))
+
+def obter_texto(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    text = soup.get_text()
+
+    return text
+
+
+def solicitacao_http(url):
+    return requests.get(url)
+
+def obter_html(url):
+    res = requests.get(url)
+    return BeautifulSoup(res.content, "html.parser")
+
+"""
+def buscar_links(url):
+    soup = obter_html(url)
+    links = soup.find_all("a")
+
+    for link in links:
+            href = link.get('href')
+            if href.startswith('http'):
+                buscar_links(href)
+            else:
+                base_url = requests.get(url).url
+                absolute_url = 
+"""
+
+
+def eh_link_externo_nao_visitado(href, urls_visitadas):
+    if href and href.startswith('http') and href not in urls_visitadas:
+        return True
+
+class localizador_de_links:
+    def __init__(self, start_url, max_depth=3):
+        self.visited_urls = set() # guarda as urls visitadas
+        self.start_url = start_url
+        self.max_depth = max_depth
+        self.session = None
+
+    def get_links(self, url):
+        session = self.get_session()
+        res = session.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        #links = soup.find_all("a")
+
+        links = []
+        for link in soup.find_all("a"):
+            href = link.get('href')
+
+            if href is not None:
+                absolute_url = self.absolute_url(href, res.url)
+                if absolute_url not in self.visited_urls:
+                    links.append(absolute_url)
+
+        return links
+
+    def get_session(self): # faz com que armazene em cache as páginas da web que foram acessadas anteriormente 
+        if not self.session:
+            requests_cache.install_cache('cache')
+            self.session = requests_cache.CachedSession() # ao obter a sessão http, todas as solicit. subseq. se está armazenada em cache
+        return self.session
+
+    def absolute_url(self, url, base_url):
+        return urljoin(base_url, url)
+
+    """
+    def absolute_url(self, url):
+        session = self.get_session()
+        res = session.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        array_links = []
+
+        for link in soup.find_all("a"): # encontrar as tags <a>
+            href = link.get('href')
+            if href is not None:
+                absolute_url = self.absolute_url(href, res.url) # criate url absoluta
+                if absolute_url not in self.visited_urls:
+                    array_links.append(absolute_url)
+        return array_links
+    """
+
+    def search(self, url=None, depth=0):
+        if not url:
+            url = self.start_url
+        if depth > self.max_depth:
+            return [] # se atingir o depth max return lista vazia
+        links = self.get_links(url)
+        self.visited_urls.add(url) # added url atual as visitadas
+
+        results = []
+        for link in links:
+            if link not in self.visited_urls:
+                results.append(link)
+                results.extend(self.search(link, depth+1))
+
+        return results
+        
+
+    """
+    def buscar_links(self, url, max_depth=0, depth=0):
+        array_links = []
+        self.visited_urls.add(url)
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+
+        links = soup.find_all("a", href==True)
+
+        if res.status_code==200:
+            text = soup.get_text()
+
+            for link in links:
+                href = link.get('href')
+
+                if eh_link_externo_nao_visitado(href, self.visited_urls):
+                    array_links.append(href)
+                    self.buscar_links(href)
+
+                elif href: # trata links relativos
+                    absolute_url = f'{res.url.rstrip("/")}/{href.lstrip("/")}' # rstrip remove caracteres
+
+                    if absolute_url not in self.visited_urls:
+                        array_links.append(href)
+                        self.buscar_links(absolute_url)
+        
+                
+                    if depth < max_depth:
+                        array_links.extend(buscar_links())
+
+        return array_links
+        """
+
+url = "http://127.0.0.1:5500/buscador/html/main.html"
+findLinks = localizador_de_links(url, 2)
+results = findLinks.search()
+print(len(results))
+for link in results:
+    print(link)
+
+"""
+def search(key, url, depth):
+    busca = localizador_de_links(url)
+
+    text = obter_texto(url)
+    soup = obter_html(url)
+    array_text = []
+    links = soup.find_all("a") # pega todos os links da página
+
+    if depth > 0:
+        
+        for link in links:
+            href = link.get('href')
+            if href.startswith('http'):
+                array_text.append(obter_texto(href))
+                
+                busca_termo(key, array_text[link])
+        
+        for i in busca:
+            busca.buscar_links(url)
+    else:
+        busca_termo(key, text)
+"""
+    #preciso buscar os links da página 0 + links de cada página até chegar no depth escolhido
+    #cuidado com requisições de paginas identicas
+
+
